35f094d9886050f017d05d0688a25a534385c302
Charles Dudley <charles.duds@gmail.com>
2024-04-28 12:17:40 -0500

    reorienting so 0,0 is bottom left

--- proj/through_the_storm/through_the_storm.pm
+++ proj/through_the_storm/through_the_storm.pm
@@ -5,7 +5,7 @@ mdp
         x : [0..7] init 0;  
         y : [0..7] init 0;
         heading : [0..3] init 3; // up=0, right=1, down=2, left=3
-        damage : [0..2] init 0; // damage sustained by aircraft
+        // damage_count : [0..2] init 0; // damage sustained by aircraft
 
         [go] heading = 0 & y < 7 ->
             .90: (y'=y+1) + 
@@ -45,54 +45,86 @@ mdp
         [turn_right] heading = 3 ->
             .90: (heading'=0) + 
             .10: (x'= x < 7 ? x + 1 : x);
-        
-
+    
     endmodule
 
 
-    // for now, storms cannot move
-    module storm70
-        present70 : bool init true;
+    // for now, storm scannot move
+    module storm07
+
+        present07 : bool init true;
 
         [go] true ->
-            .20: (present70'=!present70) + 
-            .80: (present70'=present70);
+            .20: (present07'=!present07) + 
+            .80: (present07'=present07);
         [turn_left] true ->
-            .20: (present70'=!present70) + 
-            .80: (present70'=present70);
+            .20: (present07'=!present07) + 
+            .80: (present07'=present07);
         [turn_right] true ->
-            .20: (present70'=!present70) + 
-            .80: (present70'=present70);
+            .20: (present07'=!present07) + 
+            .80: (present07'=present07);
+
     endmodule
 
-    module storm60 = storm70 [present70=present60] endmodule
+    module storm06 = storm07 [present07=present06] endmodule
     
-    module storm71 = storm70 [present70=present71] endmodule
-    module storm61 = storm70 [present70=present61] endmodule
-    module storm51 = storm70 [present70=present51] endmodule
+    module storm17 = storm07 [present07=present17] endmodule
+    module storm16 = storm07 [present07=present16] endmodule
+    module storm15 = storm07 [present07=present15] endmodule
     
-    module storm62 = storm70 [present70=present62] endmodule
-    module storm52 = storm70 [present70=present52] endmodule
-    module storm42 = storm70 [present70=present42] endmodule
+    module storm26 = storm07 [present07=present26] endmodule
+    module storm25 = storm07 [present07=present25] endmodule
+    module storm24 = storm07 [present07=present24] endmodule
     
-    module storm53 = storm70 [present70=present53] endmodule
-    module storm43 = storm70 [present70=present43] endmodule
-    module storm33 = storm70 [present70=present33] endmodule
+    module storm35 = storm07 [present07=present35] endmodule
+    module storm34 = storm07 [present07=present34] endmodule
+    module storm33 = storm07 [present07=present33] endmodule
     
-    module storm44 = storm70 [present70=present44] endmodule
-    module storm34 = storm70 [present70=present34] endmodule
-    module storm24 = storm70 [present70=present24] endmodule
+    module storm44 = storm07 [present07=present44] endmodule
+    module storm43 = storm07 [present07=present43] endmodule
+    module storm42 = storm07 [present07=present42] endmodule
     
-    module storm35 = storm70 [present70=present35] endmodule
-    module storm25 = storm70 [present70=present25] endmodule
-    module storm15 = storm70 [present70=present15] endmodule
+    module storm53 = storm07 [present07=present53] endmodule
+    module storm52 = storm07 [present07=present52] endmodule
+    module storm51 = storm07 [present07=present51] endmodule
     
-    module storm26 = storm70 [present70=present26] endmodule
-    module storm16 = storm70 [present70=present16] endmodule
-    module storm06 = storm70 [present70=present06] endmodule
+    module storm62 = storm07 [present07=present62] endmodule
+    module storm61 = storm07 [present07=present61] endmodule
+    module storm60 = storm07 [present07=present60] endmodule
     
-    module storm17 = storm70 [present70=present17] endmodule
-    module storm07 = storm70 [present70=present07] endmodule
+    module storm71 = storm07 [present07=present71] endmodule
+    module storm70 = storm07 [present07=present70] endmodule
 
 
-    // formula collision = (p=3 & b=7 | p=4 & b=4 | p=6 & b=6 | p=7 & b=3);
+    formula damage = (
+        x = 0 & y = 6 & present06 |
+        
+        x = 1 & y = 7 & present17 |
+        x = 1 & y = 6 & present16 |
+        x = 1 & y = 5 & present15 |
+        
+        x = 2 & y = 6 & present26 |
+        x = 2 & y = 5 & present25 |
+        x = 2 & y = 4 & present24 |
+        
+        x = 3 & y = 5 & present35 |
+        x = 3 & y = 4 & present34 |
+        x = 3 & y = 3 & present33 |
+        
+        x = 4 & y = 4 & present44 |
+        x = 4 & y = 3 & present43 |
+        x = 4 & y = 2 & present42 |
+        
+        x = 5 & y = 3 & present53 |
+        x = 5 & y = 2 & present52 |
+        x = 5 & y = 1 & present51 |
+        
+        x = 6 & y = 2 & present62 |
+        x = 6 & y = 1 & present61 |
+        x = 6 & y = 0 & present60 |
+        
+        x = 7 & y = 1 & present71 |
+        x = 7 & y = 0 & present70
+    );
+
+    formula reach_goal = (x = 7 & y = 7);
